e713e52f5ed6dfafbe68d33b34c79a9c71cf7b81
RippedKek <tanjeeb1909@gmail.com>
2025-02-19 01:56:36 +0600

    create login animation for login page

--- client/app/(auth)/login/page.jsx
+++ client/app/(auth)/login/page.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import { GiFallingLeaf } from 'react-icons/gi'
 
 const LoginPage = () => {
+  const [scaleX, setScaleX] = useState(false)
+
+  const handleLeafClick = () => {
+    setScaleX((prev) => !prev)
+  }
+
   return (
     <div className='w-full h-screen bg-gradient-to-b from-lightGray to-darkGray relative flex items-center justify-center'>
       <div
-        className='w-full h-screen bg-gradient-to-b from-lightGreen to-darkGreen absolute top-0 left-0'
+        className={
+          'w-full h-screen bg-gradient-to-b from-lightGreen to-darkGreen absolute top-0 left-0 transition-all duration-700'
+        }
         style={{
-          clipPath: 'polygon(0 0, 55% 0, 45% 100%, 0% 100%)',
+          clipPath: scaleX
+            ? 'polygon(0 0, 55% 0, 45% 100%, 0% 100%)'
+            : 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
           boxShadow: '10px 0px 10px rgba(0, 0, 0, 0.5)',
         }}
       ></div>
@@ -29,7 +41,10 @@ const LoginPage = () => {
             />
           </form>
         </div>
-        <GiFallingLeaf className='text-leafColor text-6xl mt-8 cursor-pointer rotate-180 hover:rotate-[190deg] transition-all duration-200' />
+        <GiFallingLeaf
+          className='text-leafColor text-6xl mt-8 cursor-pointer rotate-180 hover:rotate-[190deg] transition-all duration-200'
+          onClick={handleLeafClick}
+        />
       </div>
     </div>
   )
