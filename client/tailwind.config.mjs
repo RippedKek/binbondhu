5387581524facf5ccb542743614e1ed1e3c06579
RippedKek <tanjeeb1909@gmail.com>
2025-02-19 00:46:20 +0600

    config variables for tailwind

--- client/tailwind.config.mjs
+++ client/tailwind.config.mjs
@@ -1,17 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        lightGreen: '#43A046',
+        darkGreen: '#103E13',
+        lightGray: '#3F403F',
+        darkGray: '#141513',
+        formGreen: '#D4E5C6',
+        leafColor: '#75A777',
       },
     },
   },
   plugins: [],
-};
+}
